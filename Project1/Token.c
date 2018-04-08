4e09fa70cb8a60f52792d763c987def65cb1f2f4
nade1073 <nade1073@gmail.com>
2018-04-08 21:23:15 +0300

    Add project files.

---
+++ Project1/Token.c
@@ -0,0 +1,194 @@
+#include "Token.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int currentIndex = 0;
+Node* currentNode = NULL;
+int backCounter = 0; 
+#define TOKEN_ARRAY_SIZE 100
+extern FILE *yyin, *yyout;
+/*
+* This function creates a token and stores it in the storage.
+*/
+void create_and_store_token(eTOKENS kind, int numOfLine, char* lexeme)
+{
+
+	// case 1: there is still no tokens in the storage.
+	if (currentNode == NULL)
+	{
+		printf("Create NEW LIST\n");
+		currentNode = (Node*)malloc(sizeof(Node));
+
+		if (currentNode == NULL)
+		{
+			fprintf(yyout, "\nUnable to allocate memory! \n");
+			exit(0);
+		}
+		currentNode->tokensArray = (Token*)malloc(sizeof(Token)*TOKEN_ARRAY_SIZE);
+		if (currentNode->tokensArray == NULL)
+		{
+			fprintf(yyout, "\nUnable to allocate memory! \n");
+			exit(0);
+		}
+		currentNode->prev = NULL;
+		currentNode->next = NULL;
+	}
+
+	// case 2: at least one token exsits in the storage.
+	else
+	{
+		// the array (the current node) is full, need to allocate a new node
+		if (currentIndex == TOKEN_ARRAY_SIZE - 1)
+		{
+			currentIndex = 0;
+			currentNode->next = (Node*)malloc(sizeof(Node));
+
+			if (currentNode == NULL)
+			{
+				fprintf(yyout, "\nUnable to allocate memory! \n");
+				exit(0);
+			}
+			currentNode->next->prev = currentNode;
+			currentNode = currentNode->next;
+			currentNode->tokensArray = (Token*)malloc(sizeof(Token)*TOKEN_ARRAY_SIZE);
+
+			if (currentNode->tokensArray == NULL)
+			{
+				fprintf(yyout, "\nUnable to allocate memory! \n");
+				exit(0);
+			}
+			currentNode->next = NULL;
+		}
+
+		// the array (the current node) is not full
+		else
+		{
+			currentIndex++;
+		}
+	}
+	currentNode->tokensArray[currentIndex].kind = kind;
+	currentNode->tokensArray[currentIndex].lexeme = (char*)malloc(sizeof(char)*(strlen(lexeme) + 1));
+	strcpy(currentNode->tokensArray[currentIndex].lexeme, lexeme);
+	currentNode->tokensArray[currentIndex].lineNumber = numOfLine;
+	
+
+}
+
+/*
+* This function returns the token in the storage that is stored immediately before the current token (if exsits).
+*/
+Token *back_token()
+{
+	backCounter++;
+	if (currentIndex == 0)
+	{
+		currentNode = currentNode->prev;
+		currentIndex = TOKEN_ARRAY_SIZE - 1;
+	}
+	else
+	{
+		currentIndex--;
+	}
+	return &(currentNode->tokensArray)[currentIndex];
+}
+
+/*
+* If the next token already exists in the storage (this happens when back_token was called before this call to next_token):
+*  this function returns the next stored token.
+* Else: continues to read the input file in order to identify, create and store a new token (using yylex function);
+*  returns the token that was created.
+*/
+Token* next_token()
+{
+	if (backCounter > 0)
+	{
+		if (currentIndex == TOKEN_ARRAY_SIZE - 1)
+		{
+			currentNode = currentNode->next;
+			currentIndex = 0;
+		}
+		else
+		{
+			currentIndex++;
+		}
+		backCounter--;
+	}
+	else
+	{	
+		yylex();
+	}
+	return &(currentNode->tokensArray[currentIndex]);
+}
+
+char *stringFromeTOKENS(enum eTOKENS f)
+{
+	static const char *strings[] =
+	{
+		"AR_OP_ADDITION",
+		"AR_OP_SUBTRACTION",
+		"AR_OP_MULTIPLICATION",
+		"AR_OP_DIVISION",
+		"AR_OP_POWER",
+		"UNARY_OP_INCREMENT",
+		"UNARY_OP_AMP",
+		"REL_OP_LESS",
+		"REL_OP_EQUAL_OR_LESS",
+		"REL_OP_EQUAL",
+		"REL_OP_NOT_EQUAL",
+		"REL_OP_GREATER",
+		"REL_OP_EQUAL_OR_GREATER",
+		"ASSIGNMENT",
+		"POINTER",
+		"ID",
+		"INT_NUM",
+		"REL_NUM",
+		"KEYWORD_BLOCK",
+		"KEYWORD_BEGIN",
+		"KEYWORD_END",
+		"KEYWORD_TYPE",
+		"KEYWORD_IS",
+		"KEYWORD_INTEGER",
+		"KEYWORD_REAL",
+		"KEYWORD_ARRAY",
+		"KEYWORD_OF",
+		"KEYWORD_WHEN",
+		"KEYWORD_DO",
+		"KEYWORD_DEFAULT",
+		"KEYWORD_END_WHEN",
+		"KEYWORD_FOR",
+		"KEYWORD_END_FOR",
+		"KEYWORD_FREE",
+		"KEYWORD_SIZE_OF",
+		"SEPERATION_SIGN_COLON",
+		"SEPERATION_SIGN_SEMICOLON",
+		"SEPERATION_SIGN_PARENT_OPEN",
+		"SEPERATION_SIGN_PARENT_CLOSE",
+		"SEPERATION_SIGN_BRACKET_OPEN",
+		"SEPERATION_SIGN_BRACKET_CLOSE",
+		"END_OF_FILE"
+	};
+	return strings[f];
+}
+
+void resetVarsAndList()
+{
+	printf("Reset Values And List\n");
+	yyrestart();
+	Node* saveNextNode;
+	backCounter = 0;
+	currentIndex = 0;
+	while (currentNode->prev != NULL)
+	{
+		currentNode = currentNode->prev;
+	}
+	while (currentNode != NULL)
+	{
+		free(currentNode->tokensArray);
+		saveNextNode = currentNode->next;
+		free(currentNode);
+		currentNode = saveNextNode;
+	}
+	printf("Sucsses Reset Value and List\n");
+	
+}
---
+++ Project1/Token.h
@@ -0,0 +1,73 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+
+
+typedef enum eTOKENS
+{
+	AR_OP_ADDITION,
+	AR_OP_SUBTRACTION,
+	AR_OP_MULTIPLICATION,
+	AR_OP_DIVISION,
+	AR_OP_POWER,
+	UNARY_OP_INCREMENT,
+	UNARY_OP_AMP,
+	REL_OP_LESS,
+	REL_OP_EQUAL_OR_LESS,
+	REL_OP_EQUAL,
+	REL_OP_NOT_EQUAL,
+	REL_OP_GREATER,
+	REL_OP_EQUAL_OR_GREATER,
+	ASSIGNMENT,
+	POINTER,
+	ID,
+	INT_NUM,
+	REL_NUM,
+	KEYWORD_BLOCK,
+	KEYWORD_BEGIN,
+	KEYWORD_END,
+	KEYWORD_TYPE,
+	KEYWORD_IS,
+	KEYWORD_INTEGER,
+	KEYWORD_REAL,
+	KEYWORD_ARRAY,
+	KEYWORD_OF,
+	KEYWORD_WHEN,
+	KEYWORD_DO,
+	KEYWORD_DEFAULT,
+	KEYWORD_END_WHEN,
+	KEYWORD_FOR,
+	KEYWORD_END_FOR,
+	KEYWORD_FREE,
+	KEYWORD_SIZE_OF,
+	SEPERATION_SIGN_COLON,
+	SEPERATION_SIGN_SEMICOLON,
+	SEPERATION_SIGN_PARENT_OPEN,
+	SEPERATION_SIGN_PARENT_CLOSE,
+	SEPERATION_SIGN_BRACKET_OPEN,
+	SEPERATION_SIGN_BRACKET_CLOSE,
+	END_OF_FILE
+
+}eTOKENS;
+
+typedef struct Token
+{
+	eTOKENS kind;
+	char *lexeme;
+	int lineNumber;
+}Token;
+
+typedef struct Node
+{
+	Token *tokensArray;
+	struct Node *prev;
+	struct Node *next;
+} Node;
+
+void create_and_store_token(eTOKENS kind, int numOfLine, char* lexeme);
+Token *next_token();
+Token *back_token();
+
+char* stringFromeTOKENS(enum eTOKENS f);
+void resetVarsAndList();
+#endif
